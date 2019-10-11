80290efd5192d4f37b821cd7dffe241dcbef8147
MinaFes <ribeirocatarina72@gmail.com>
2019-10-11 15:05:09 +0200

    Modification du tp 1 concernant le menu

--- 2-File.py
+++ 2-File.py
@@ -1,58 +1,54 @@
-# les menus
-def menu():
-    print('--------------menu----------------')
-    print("1. Choisir un nom de fichier")
+#création du fichier
+def crer():
+    nomFichier = str(input("Nom du fichier : "))
+    fichier = open(nomFichier, "w+")
+    fichier.close()
+
+#écriture dans le fichier
+def ecrire():
+    nomFichier = str(input("Nom du fichier : "))
+    fichier = open(nomFichier, "a")
+    fichier.write("\n"+str(input("Entrez le texte : \n")))
+    fichier.close()
+
+#lecture du fichier
+def lire():
+    nomFichier = str(input("Nom du fichier : "))
+    fichier = open(nomFichier, "r")
+    print(fichier.read())
+    fichier.close()
+
+#vider le contenu du fichier
+def vider():
+    nomFichier = str(input("Nom du fichier : "))
+    fichier = open(nomFichier, "w")
+    fichier.close()
+
+#Création du menu
+def main():
+    print("1. Choisir un nom de fichier ")
     print("2. Ajouter un texte")
     print("3. Afficher le fichier complet")
     print("4. Vider le fichier")
-    print("5. Quitter le programme")
-    print('-------------menu----------------\n')
+    print("9. Quitter")
 
-print('------------------------------')
-print("      Fichier Application    ")
-print('------------------------------\n')
+    choix = 0
 
-a=1
-while (a):
-    menu()
-    c = int(input(" Entrez votre choix de fonction:"))
-    print(c)
+    while choix != 9:
+        choix = int(input("Entrez votre choix : "))
 
-    if c==1:
-        print('------------Choisir un nom:------------------')
-        print("a. fichierA")
-        print("b. fichierB")
-        print("c. fichierC")
-        print('------------Choisir un nom------------------\n')
-        nomChois=input(" Entrez votre choix de nom de ficher:")
-        if nomChois == "a":
-            nomFicher = "fichierA.txt"
-        elif nomChois == "b":
-            nomFicher = "fichierB.txt"
-        elif nomChois == "c":
-            nomFicher = "fichierC.txt"
-        #ficher=open(nomFicher,"wt")
-        #with open(nomFicher, "r") as ficher:
-            print('nom de fichier est:',nomFicher)
-        print('------------------------------\n')
-    elif c==2:
-        with open(nomFicher, "a+") as ficher:
-        #ficher=open(nomFicher,"wt")
-            texte=input("Ajouter un texte:")
-            ficher.write(texte)
-            print("Vous avez terminé d'ajouter")
-        #ficher.close()
-        print('------------------------------\n')
-    elif c==3:
-        with open(nomFicher, "r") as ficher:
-            texteAfficher = ficher.read();
-            print("Le contenu du fichier est: %s" % (texteAfficher))
-        print('------------------------------\n')
-    elif c== 4:
-        with open(nomFicher, "r") as ficher:
-            print("Le fichier a été vidé")
-        print('------------------------------\n')
-    elif c==5:
-        a=0
+        if choix == 1:
+            crer()
+        elif choix == 2:
+            ecrire()
+        elif choix == 3:
+            lire()
+        elif choix == 4:
+            vider()
+        elif choix == 9:
+            exit()
+        else:
+            print("Choix non valide!")
 
-
+if __name__ == '__main__':
+    main()
